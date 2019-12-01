a40c45864688c91cbd3eb2de1b651c6b936f2b3a
Jacob Krall <jacob@jkrall.net>
2019-12-01 11:39:25 -0500

    day 1: stream operator goes at beginning of line?

--- day1.exs
+++ day1.exs
@@ -9,8 +9,8 @@ defmodule Day1 do
     end
 end
 
-IO.puts IO.stream(:stdio, :line) |>
-    Stream.map(&String.trim/1) |>
-    Stream.map(&String.to_integer/1) |>
-    Stream.map(&Day1.fuel/1) |>
-    Enum.sum
+IO.puts IO.stream(:stdio, :line)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.map(&Day1.fuel/1)
+    |> Enum.sum
