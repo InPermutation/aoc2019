6b6d36c8397f5ec6d16d8a487b1a37d77fc33f2a
Jacob Krall <jacob@jkrall.net>
2019-12-01 11:37:28 -0500

    day 1: use arity instead of placeholder variable

--- day1.exs
+++ day1.exs
@@ -10,7 +10,7 @@ defmodule Day1 do
 end
 
 IO.puts IO.stream(:stdio, :line) |>
-    Stream.map(&String.trim(&1)) |>
-    Stream.map(&String.to_integer(&1)) |>
-    Stream.map(&Day1.fuel(&1)) |>
+    Stream.map(&String.trim/1) |>
+    Stream.map(&String.to_integer/1) |>
+    Stream.map(&Day1.fuel/1) |>
     Enum.sum
