c00594f560c67ebcf65f41e20d42fe5785e58a23
Jacob Krall <jacob@jkrall.net>
2019-12-07 15:14:39 -0500

    day 6, part 1: count orbits to COM

---
+++ day6.exs
@@ -0,0 +1,27 @@
+defmodule Day6 do
+
+    def update_map(line, map) do
+        [body, satellite] = String.trim(line) |> String.split(")")
+        Map.put(map, satellite, body)
+    end
+
+    def count_orbits(map) do
+        Map.keys(map)
+            |> Enum.map(&(hops_to_com(&1, map)))
+            |> Enum.sum
+    end
+
+    def hops_to_com("COM", _), do: 0
+    def hops_to_com(satellite, map) do
+        next = Map.get(map, satellite)
+        1 + hops_to_com(next, map)
+    end
+
+end
+
+map = IO.stream(:stdio, :line)
+    |> Enum.reduce(%{}, &Day6.update_map/2)
+
+Day6.count_orbits(map)
+    |> IO.inspect
+
