30000883875ab44329a4b0608340316ba86a4b8a
Jacob Krall <jacob@jkrall.net>
2019-12-08 17:51:24 -0500

    Day 8, part 2: compositing layers in SIF

--- day8.exs
+++ day8.exs
@@ -8,14 +8,41 @@ defmodule Day8 do
     def one_by_two(layer) do
         count(layer, ?1) * count(layer, ?2)
     end
+
+    def composite(fore, aft) do
+        Enum.zip(fore, aft)
+            |> Enum.map(&composite_one/1)
+    end
+
+    def composite_one({fore_pix, aft_pix}) do
+        if fore_pix == ?2 do
+            aft_pix
+        else
+            fore_pix
+        end
+    end
+
+    def puts(s) do
+        Enum.map(s, fn ch -> if ch == ?1 do '*' else ' ' end end)
+        |> IO.puts
+    end
 end
+
+[columns, rows] = [25, 6]
 
 layers = IO.read(:stdio, :all)
     |> String.trim
     |> String.to_charlist
-    |> Enum.chunk_every(25 * 6)
+    |> Enum.chunk_every(columns * rows)
 
 layers
     |> Enum.min_by(&Day8.count_zeroes/1)
     |> Day8.one_by_two
     |> IO.inspect(label: "Part 1")
+
+IO.puts("Part 2:")
+layers
+    |> Enum.reverse
+    |> Enum.reduce(&Day8.composite/2)
+    |> Enum.chunk_every(columns)
+    |> Enum.each(&Day8.puts/1)
