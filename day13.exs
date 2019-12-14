2afe7ab07ebf5a54e963fa8f17919d47b2aa173c
Jacob Krall <jacob@jkrall.net>
2019-12-14 13:00:43 -0500

    day 13, part 2: play a full game.... slowly

--- day13.exs
+++ day13.exs
@@ -1,4 +1,87 @@
 defmodule Day13 do
+    def xrange(), do: 40
+    def yrange(), do: 24
+
+    def play(state, screen \\ init_screen(), score \\ 0) do
+        cond do
+            state.halted -> %{ score: score }
+            Enum.count(state.output) == 3 ->
+                modify_then_play(state, screen, score)
+            true ->
+                play(step(state), screen, score)
+        end
+    end
+
+    def modify_then_play(state, screen, score) do
+        [x, y, v] = state.output
+        if x == -1 && y == 0 do
+            newstate = %{ state | output: [] }
+            play(newstate, screen, v)
+        else
+            if find_ball(screen) && find_paddle(screen) && false do
+                display!(screen)
+                IO.inspect([bx: find_ball(screen), px: find_paddle(screen), d: follow_ball(screen)])
+            end
+            newscreen = update_screen(state.output, screen)
+            newstate = %{ state | input: [ follow_ball(newscreen) ], output: []}
+            play(newstate, newscreen, score)
+        end
+    end
+
+    def display!(screen) do
+        Enum.each(screen, fn row ->
+            Enum.map(row, fn v ->
+                case v do
+                    0 -> ' '
+                    1 -> '+'
+                    2 -> '#'
+                    3 -> '-'
+                    4 -> '*'
+                end
+            end)
+            |> IO.puts
+        end)
+    end
+
+    def follow_ball(screen) do
+        bx = find_ball(screen)
+        px = find_paddle(screen)
+
+        cond do
+            bx > px -> +1
+            bx < px -> -1
+            true -> 0
+        end
+    end
+
+    def find_ball(screen), do: find(screen, 4)
+    def find_paddle(screen), do: find(screen, 3)
+
+    def find([], _tid), do: nil
+    def find([row | rest], tid) do
+        ix = Enum.find_index(row, fn v -> v == tid end)
+        if ix == nil do
+            find(rest, tid)
+        else
+            ix
+        end
+    end
+
+    def init_screen() do
+        row = List.duplicate(0, xrange())
+        List.duplicate(row, yrange())
+    end
+
+    def update_screen([x, y, tid], screen) do
+        if y >= 0 && y < yrange() && x >= 0 && x <= xrange() do
+            row = Enum.fetch!(screen, y)
+                |> List.replace_at(x, tid)
+                List.replace_at(screen, y, row)
+        else
+            raise "validation failed"
+        end
+    end
+
     def run(state) do
         if state.halted do
             state
@@ -157,25 +240,6 @@ defmodule Day13 do
 
         [de, c, b, a]
     end
-
-    def visualize({hull, _loc, _dir}) do
-        keys = Map.keys(hull)
-        {x_min, x_max} = Enum.min_max(Enum.map(keys, fn {x, _y} -> x end))
-        {y_min, y_max} = Enum.min_max(Enum.map(keys, fn {_x, y} -> y end))
-
-        for y <- y_min..y_max do
-            for x <- x_min..x_max do
-                pix = case hull[{x, y}] do
-                    nil -> ' '
-                    0 -> ' '
-                    1 -> '#'
-                end
-                IO.write(pix)
-            end
-            IO.puts("")
-        end
-
-    end
 end
 
 rom = IO.gets("")
@@ -190,3 +254,8 @@ state.output
     |> Enum.filter(fn [_x, _y, tile_id] -> tile_id == 2 end)
     |> Enum.count
     |> IO.inspect(label: "Part 1")
+
+quarters = Day13.init(List.replace_at(rom, 0, 2), [])
+
+Day13.play(quarters)
+    |> IO.inspect(label: "Part 2")
